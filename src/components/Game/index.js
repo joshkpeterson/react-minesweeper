efd0c7cbe3d4bd86a70d6eba992a61a8c813e7f1
Josh <joshkpete@gmail.com>
2020-08-26 15:30:32 -0400

    Add getNeighbors() draft

--- src/components/Game/index.js
+++ src/components/Game/index.js
@@ -4,7 +4,9 @@ import styles from './game.module.scss';
 import Cell from 'components/Cell'
 import { CELL_TYPES } from 'utils/constants'
 
-const gridSize = 24 * 20;
+const rowLength = 24;
+const columnLength = 20;
+const gridSize = rowLength * columnLength;
 const cellDataStub = {
   nearBombCount: 3,
   type: CELL_TYPES.COUNT,
@@ -23,6 +25,7 @@ function Game() {
   let isPause = false;
 
   const [ gameData, setGameData ] = useState(tempGameData);
+  const [ neighbors, setNeighbors ] = useState();
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -34,6 +37,59 @@ function Game() {
     return () => clearInterval(interval);
   }, [isPause]);
 
+  // useEffect(() => {
+  //   setNeighbors = getNeighbors()
+  // }, []);
+
+  const getNeighbors = (index) => {
+    let indexArray = [];
+    let isTop = true,
+        isBottom = true,
+        isLeft = true,
+        isRight = true;
+    let remainder = index % rowLength;
+    // Not left column
+    if (remainder === 0) {
+      indexArray.push(gameData[index - 1]);
+      isLeft = false;
+    }
+    // Not right column
+    if (remainder === 0) {
+      indexArray.push(gameData[index + 1]);
+      isRight = false;
+    }
+    // Not top row
+    if (index - rowLength > -1) {
+      indexArray.push(gameData[index - rowLength]);
+      isTop = false;
+    }
+    // Not bottom row
+    if (index + rowLength > gridSize) {
+      indexArray.push(gameData[index - rowLength]);
+      isBottom = false;
+    }
+
+    // top left
+    if (!isTop && !isLeft) {
+      indexArray.push(gameData[index - rowLength - 1]);
+    }
+    // top right
+    if (!isTop && !isRight) {
+      indexArray.push(gameData[index - rowLength + 1]);
+    }
+    // bottom left
+    if (!isBottom && !isLeft) {
+      indexArray.push(gameData[index + rowLength - 1]);
+    }
+    // bottom right
+    if (!isBottom && !isRight) {
+      indexArray.push(gameData[index + rowLength + 1]);
+    }
+
+
+    return indexArray.map(i => gameData[i])
+  }
+
   return (
     <div className={styles.game_container}>
       <div className={styles.game__header}>
@@ -44,7 +100,7 @@ function Game() {
         <div className={styles.game__clock}>{seconds}</div>
       </div>
       <div className={styles.game__grid}>
-        {gameData.map((cellData, index) => <Cell key={index} data={cellData}/>)}
+        {gameData.map((cellData, index) => <Cell key={index} data={cellData} neighbors={getNeighbors(index)} />)}
       </div>
     </div>
   );
