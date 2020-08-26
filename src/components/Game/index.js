19fee370d98c568474d730980f3fddc2a374e1bf
Josh <joshkpete@gmail.com>
2020-08-26 03:31:08 -0400

    Add create initial game state and list render

--- src/components/Game/index.js
+++ src/components/Game/index.js
@@ -1,11 +1,27 @@
 import React, { useState, useEffect } from 'react';
+import { clone } from 'lodash';
 import styles from './game.module.scss';
 
+
+const gridSize = 40;
+const cellDataStub = {
+  nearBombCount: 0,
+  isBomb: false,
+  isRevealed: false,
+}
+let tempGameData = [];
+
+// Create initial game state for each cell
+for (var i = 0; i < gridSize; i++) {
+    tempGameData.push(clone(cellDataStub));
+}
+
 function Game() {
-  // let seconds = 0;
   const [ bombCount, setBombCount ] = useState(20);
   const [ seconds, setSeconds ] = useState(0);
   let isPause = false;
+
+  const [ gameData, setGameData ] = useState(tempGameData);
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -25,6 +41,9 @@ function Game() {
         </div>
         <button className={styles.game__reset_button} onClick={()=> setSeconds(0)}>Reset</button>
         <div className={styles.game__clock}>{seconds}</div>
+        <div className={styles.game__grid}>
+          {gameData.map((cellData) => <div>foo</div>)}
+        </div>
       </div>
     </div>
   );
