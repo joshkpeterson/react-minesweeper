c2c90193c48101fc2b98422d9bb96f00c79b01de
Josh <joshkpete@gmail.com>
2020-08-25 23:10:08 -0400

    Add scss and modules demo

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { appLink } from './app.module.scss';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className={appLink}
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
